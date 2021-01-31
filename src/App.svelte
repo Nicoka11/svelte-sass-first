bed2dd5e097cfd4fa4867413c7ebf379f1ed4501
Nicoka11 <nicoka11@yahoo.fr>
2021-01-31 19:44:29 +0100

    v1

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+
+<script>
+  import Nav from './components/Nav.svelte'
+  let name = "Maïli-Anna";
+  let likes = 0
+  $: doubledLiked = likes * 2
+
+  function btnLikesHandler () {
+    likes++
+  }
+</script>
+
+<main>
+  <h1>Hello {name.toUpperCase()}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <Nav/>
+  <button on:click={btnLikesHandler}>Liked {likes} {likes < 2 ? 'Time' : 'Times'}</button>
+  <p>Likes * 2 is {doubledLiked}</p>
+</main>
+
+<style lang="scss">
+  $main-color: rgb(221, 63, 0);
+  
+  main {
+    text-align: center;
+    max-width: 800px;
+    margin: 0 auto;
+
+    h1 {
+      color: blue;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
+
+    p {
+      color: rgb(51, 51, 51);
+    }
+  }
+
+  @media (min-width: 1000px) {
+    main {
+      max-width: none;
+
+      h1 {
+        color: $main-color;
+      }
+    }
+  }
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,30 @@
+<ul class="nav-list">
+  <li>Home</li>
+  <li>Our Services</li>
+  <li>Retail Shops</li>
+  <li>About</li>
+</ul>
+
+<style lang="scss">
+  $main-color: rgb(221, 63, 0);
+
+  .nav-list {
+    color: black;
+    font-size: 1.5rem;
+  }
+
+  li {
+    background-color: rgba($color: $main-color, $alpha: 0.1);
+    text-transform: uppercase;
+    display: inline-block;
+    list-style: none;
+    padding: 20px;
+    border: 2px solid $main-color;
+    border-radius: 6px;
+    width: 200px;
+
+    &:not(:last-child) {
+      margin-right: 5px;
+    }
+  }
+</style>
